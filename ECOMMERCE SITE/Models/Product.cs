8cb9ca424d2fc8d8d50e1738f91dbc2dd408d099
ETHANCPIPKIN <lucidfridaynights@gmail.com>
2021-08-20 23:51:47 -0700

    created the product model

---
+++ ECOMMERCE SITE/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECOMMERCE_SITE.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        /// <summary>
+        /// the consumer facing name of the product
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// the retail price of the product 
+        /// </summary>
+        public double Price { get; set; }
+        /// <summary>
+        /// The category of the product
+        /// </summary>
+        public string Category { get; set; }
+    }
+}
