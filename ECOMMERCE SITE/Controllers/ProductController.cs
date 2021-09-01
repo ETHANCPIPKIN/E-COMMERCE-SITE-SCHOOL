71e325990d6c74748f27b631da3596814046e083
ETHANCPIPKIN <lucidfridaynights@gmail.com>
2021-08-31 22:50:22 -0700

    staging extra changes

---
+++ ECOMMERCE SITE/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using ECOMMERCE_SITE.Models;
+using ECOMMERCE_SITE.Models.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECOMMERCE_SITE.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductContext _context; 
+        public ProductController(ProductContext context)
+        {
+            _context = context; 
+        }
+        ///Displays a view that lists all products
+        public IActionResult Index()
+        {
+            // get all products
+            List<Product> products = _context.Products.ToList();
+            
+
+            // send list of products to view to be displayed
+            return View(products);
+        }
+    }
+}
