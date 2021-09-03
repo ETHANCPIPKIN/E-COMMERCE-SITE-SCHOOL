d02915379fe0e7745d416fe6c14278517241aaaf
ETHANCPIPKIN <lucidfridaynights@gmail.com>
2021-09-03 02:53:40 -0700

    finished shopping cart best I could

--- ECOMMERCE SITE/Controllers/CartController.cs
+++ ECOMMERCE SITE/Controllers/CartController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECOMMERCE_SITE.Models;
+using ECOMMERCE_SITE.Models.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +12,48 @@ using System.Threading.Tasks;
 namespace ECOMMERCE_SITE.Controllers
 {
     public class CartController : Controller
-    {
-        public IActionResult Add(int id)
+    { 
+
+        private readonly ProductContext _context;
+        private readonly IHttpContextAccessor _httpContext;
+        public CartController(ProductContext context, IHttpContextAccessor httpContext)
         {
-            return View();
+            _context = context;
+            _httpContext = httpContext;
         }
+        public async Task<IActionResult> Add(int id)
+        {
+            Product p = await (from products in _context.Products
+                               where products.ProductId == id
+                               select products).SingleAsync();
+
+            string existingItems = _httpContext.HttpContext.Request.Cookies["CartCookie"];
+
+            List<Product> cartProducts = new List<Product>();
+            if(existingItems != null)
+            {
+              cartProducts = JsonConvert.DeserializeObject<List<Product>>(existingItems); 
+            }
+
+            cartProducts.Add(p);
+            string data = JsonConvert.SerializeObject(p);
+            CookieOptions options = new CookieOptions()
+            {
+                Expires = DateTime.Now.AddYears(1),
+                Secure = true,
+                IsEssential = true
+            };
+
+            _httpContext.HttpContext.Response.Cookies.Append("CartCookie", data, options); 
+
+            return RedirectToAction("Index", "Product");
+        }
+
+        private void SingleorDefaultAsync()
+        {
+            throw new NotImplementedException();
+        }
+
         public IActionResult Summary()
         {
             return View();
