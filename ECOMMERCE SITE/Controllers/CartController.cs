54534014e09ed246eed13d0520e350dd5370f7a6
ETHANCPIPKIN <lucidfridaynights@gmail.com>
2021-09-03 02:07:05 -0700

    added shopping cart link to navigation

---
+++ ECOMMERCE SITE/Controllers/CartController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECOMMERCE_SITE.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Add(int id)
+        {
+            return View();
+        }
+        public IActionResult Summary()
+        {
+            return View();
+        }
+    }
+}
