135e44a5fe72e046d4f5736c8c82f0a60970ce42
Ethan Pipkin <75825834+ETHANCPIPKIN@users.noreply.github.com>
2021-09-03 01:16:15 -0700

    Issue#login registration (#17)
    * Added user model, updated context
    
    * made registration page
    
    * Added register user into db
    
    * Created log in page
    
    * enabled sessions
    
    * fiinished log in function

---
+++ ECOMMERCE SITE/Controllers/UserController.cs
@@ -0,0 +1,74 @@
+﻿using ECOMMERCE_SITE.Models;
+using ECOMMERCE_SITE.Models.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECOMMERCE_SITE.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ProductContext _context;
+        
+        public UserController(ProductContext context)
+        {
+            _context = context; 
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel reg)
+        {
+            if (ModelState.IsValid)
+            {
+                User User = new User()
+                {
+                    DateOfBirth = reg.DateOfBirth,
+                    Email = reg.Email,
+                    Password = reg.Password,
+                    UserName = reg.Username
+                };
+                _context.Users.Add(User);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index", "Home"); 
+            }
+
+            return View(reg);
+        }
+
+        public IActionResult Login()
+        {
+
+            if(HttpContext.Session.GetInt32("UserId").HasValue)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            User account = await
+                (from u in _context.Users
+                 where u.UserName == model.Username
+                 && u.Password == model.Password
+                 select u).SingleOrDefaultAsync();
+            if(account == null)
+            {
+                return View(model);
+            }
+
+            HttpContext.Session.SetInt32("UserId", account.UserId);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ ECOMMERCE SITE/Models/User.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECOMMERCE_SITE.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+        
+        public DateTime? DateOfBirth { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Username { get; set; }
+
+        [Compare(nameof(Email))]
+        [Required]
+        [Display(Name ="Confirm Email")]
+        [StringLength(200)]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(120, MinimumLength =6)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Key]
+
+        public int id { get; set; }
+        [Required]
+        [Display(Name = "Username or Email")]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
